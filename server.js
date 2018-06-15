84e34562ce2c8bad424d0c7dee8f7a66c2aa4998
Jonathan Moss <nathaniel.coldfyre@gmail.com>
2018-06-15 13:40:49 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,94 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var cors = require("cors");
+var ObjectID = mongodb.ObjectID;
+
+var USER_PROFILES = "profiles";
+
+var USER_REVIEWS = "reviews";
+
+var app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://admin:Maximum_Effort2@ds261460.mlab.com:61460/maximum_effort",
+  function(err, client) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+
+    // Save database object from the callback for reuse.
+    db = client.db();
+    console.log("Database connection ready");
+
+    // Initialize the app.
+    var server = app.listen(process.env.PORT || 8080, function() {
+      var port = server.address().port;
+      console.log("App now running on port", port);
+    });
+  }
+);
+
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({ error: message });
+}
+
+app.get("/api/profiles", function(req, res) {
+  db.collection(USER_PROFILES)
+    .find({})
+    .toArray(function(err, docs) {
+      if (err) {
+        handleError(res, error.message, "Failed to get profiles.");
+      } else {
+        res.status(200).json(docs);
+      }
+    });
+});
+
+app.post("/api/profiles", function(req, res) {
+  console.log("adding profile");
+  console.log(req.body);
+  var newProfile = req.body;
+  if (!req.body.name) {
+    handlError(res, "Invalid Input", "Invalid Input", 400);
+  }
+
+  db.collection(USER_PROFILES).insertOne(newProfile, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new profile.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+app.delete("/api/profiles/:userID", function(req, res) {
+  console.log(parseInt(req.params.userID));
+  db.collection(USER_PROFILES).deleteOne(
+    { _id: new mongodb.ObjectID(req.params.userID) },
+    function(err, result) {
+      if (err) {
+        console.log("failed");
+      }
+      console.log("success");
+      console.log(result);
+      console.log(result.deletedCount);
+      res.status(200).json(req.params.userID);
+    }
+  );
+});
+
+/* app.get("/api/page1", function(req, res) {});
+
+app.get("/api/page2", function(req, res) {});
+
+app.get("/api/page3", function(req, res) {}); */
