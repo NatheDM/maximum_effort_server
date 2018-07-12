d194055a1a41fbe064a7b4c660c682c688965379
Jonathan Moss <nathaniel.coldfyre@gmail.com>
2018-07-12 15:02:16 -0700

    it works

--- routes/api/Profile.js
+++ routes/api/Profile.js
@@ -4,17 +4,17 @@ var Profile = mongoose.model("Profile");
 
 router.get("/", (req, res, next) => {
   return Profile.find().then(prfl => {
-    console.log(prfl);
+    // console.log(prfl);
     return res.json(prfl);
   });
 });
 
 router.get(":id", (req, res, next) => {
   return Profile.find(prfl => {
-    console.log(req.params.id);
+    // console.log(req.params.id);
     return prfl._id == parseInt(req.params.id);
   }).then(prfl => {
-    console.log(res.json(prfl));
+    // console.log(res.json(prfl));
     return res.json(prfl);
   });
 });
@@ -33,23 +33,6 @@ router.post("/", (req, res, next) => {
     password: req.body.password,
     interest: req.body.interest
   });
-
-  /*   router.post(":id", (req, res, next) => {
-    let prfl = new Profile({
-      nameFirst: req.body.nameFirst,
-      nameLast: req.body.nameLast,
-      nameUser: req.body.nameUser,
-      dateBirth: req.body.dateBirth,
-      homeCity: req.body.homeCity,
-      homeState: req.body.homeState,
-      homeZip: req.body.homeZip,
-      email: req.body.email,
-      password: req.body.password,
-      interest: req.body.interest
-    });
-  }); */
-
-  // console.log(prfl);
 
   prfl.save(err => {
     if (err) console.log(err);
